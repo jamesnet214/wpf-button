214402e431a15a484fc574b6db3024f76c1cbe7f
james.lee <james.lee@devncore.org>
2021-04-13 23:50:03 +0900

    Update

---
+++ DevNcoreButtonExample/GitHubControls/GitHubButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GitHubControls
+{
+    public class GitHubButton : Button
+    {
+        static GitHubButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GitHubButton), new FrameworkPropertyMetadata(typeof(GitHubButton)));
+        }
+    }
+}
